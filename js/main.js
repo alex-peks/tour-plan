dcb198b1e61585c07d9310f3ee86f6bd56312216
alex-peks <alexandrjob1@gmail.com>
2021-11-04 15:33:46 +0300

    added: mask for input phone

--- js/main.js
+++ js/main.js
@@ -50,9 +50,36 @@ closeModalButton.on('click', closeModal);
     var modalDialog = $('.modal__dialog');
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
-  }
+  };
+
+// Обработка форм
+$(".form").each(function () {
+  $(this).validate({
+  errorClass: "invalid",
+  messages: {
+    name: {
+      required: "Please specify your name",
+      minlength: "The name must be at least 2 characters long",
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com",
+    },
+    phone: {
+      required: 'The phone number is required to fill in.',
+    },
+  },
+});
+  
+});
 
 
+$(document).ready(function(){
+  $("#phone").mask("+7 (999) 999-99-99");
+});
 
+$(document).ready(function(){
+  $("#phone-2").mask("+7 (999) 999-99-99");
+});
 
 });
